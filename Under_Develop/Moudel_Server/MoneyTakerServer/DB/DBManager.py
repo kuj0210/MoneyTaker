67d1a01eaa1981acec873279498ff0a07489861e
현진혁 <t7397281@naver.com>
2018-09-16 05:26:53 +0900

    서버. sqlite에서 pymysql로 dbManager 클래스 변경

--- Under_Develop/Moudel_Server/MoneyTakerServer/DB/DBManager.py
+++ Under_Develop/Moudel_Server/MoneyTakerServer/DB/DBManager.py
@@ -5,7 +5,7 @@ http://pythonstudy.xyz/python/article/202-MySQL-%EC%BF%BC%EB%A6%AC
 #2차 수정 : hyeon jin-hyeok.
 #변경 내용 : 클래스화.
 """
-import sqlite3
+import pymysql
 import os
 
 class DBManager:
@@ -14,67 +14,103 @@ class DBManager:
         if not os.path.isfile("%s.db" %(dbn)):
             self.initDB()
 
-    # TODO : 해당 테이블 만드는 부분을 따로 분리할 필요가 있음. mariaDB로 옮긴 후에는 해당 initDB에 pass 로 치환할 것.
     def initDB(self):
+        pass
         querys = [
+            """drop database if exists moneytaker;""",
+            """create database moneytaker
+            default character set utf8
+            default collate utf8_general_ci;""",
+            """use moneytaker;""",
             """CREATE TABLE mt_user
             (
-                `index`  INT            NOT NULL    AUTO_INCREMENT, 
-                `id`     VARCHAR(45)    NOT NULL, 
-                `pw`     VARCHAR(45)    NOT NULL, 
-                `token`  CHAR(256)      NOT NULL, 
-                `class`  CHAR(45)       NULL, 
-                PRIMARY KEY (index)
-            );""",
+                `idx`    INT             NOT NULL    AUTO_INCREMENT, 
+                `id`     VARCHAR(45)     NOT NULL, 
+                `pw`     VARCHAR(256)    NOT NULL, 
+                `token`  VARCHAR(256)    NOT NULL, 
+                `class`  VARCHAR(45)     NULL, 
+                PRIMARY KEY (idx)
+            )ENGINE=InnoDB
+            default character set utf8
+            default collate utf8_general_ci;""",
             """CREATE TABLE mt_group
             (
-                `gindex`  INT            NOT NULL    AUTO_INCREMENT, 
-                `name`    VARCHAR(45)    NOT NULL, 
-                `state`   VARCHAR(45)    NULL, 
-                PRIMARY KEY (gindex)
-            );""",
+                `gidx`   INT            NOT NULL    AUTO_INCREMENT, 
+                `name`   VARCHAR(45)    NOT NULL, 
+                `state`  VARCHAR(45)    NULL, 
+                PRIMARY KEY (gidx)
+            )ENGINE=InnoDB
+            default character set utf8
+            default collate utf8_general_ci;""",
             """CREATE TABLE mt_iou
             (
-                `rindex`          INT               NOT NULL    AUTO_INCREMENT, 
-                `date`            TIMESTAMP         NOT NULL, 
-                `borrower_index`  INT               NOT NULL, 
-                `lender_index`    INT               NOT NULL, 
-                `start_date`      DATETIME          NULL, 
-                `end_date`        DATETIME          NULL, 
-                `cost`            DECIMAL(18, 0)    NOT NULL, 
-                `state`           INT               NULL, 
-                PRIMARY KEY (rindex)
-            );""",
+                `ridx`          INT               NOT NULL    AUTO_INCREMENT, 
+                `date`          TIMESTAMP         NOT NULL, 
+                `borrower_idx`  INT               NOT NULL, 
+                `lender_idx`    INT               NOT NULL, 
+                `start_date`    DATETIME          NULL, 
+                `end_date`      DATETIME          NULL, 
+                `cost`          DECIMAL(18, 0)    NOT NULL, 
+                `state`         INT               NULL, 
+                PRIMARY KEY (ridx)
+            )ENGINE=InnoDB
+            default character set utf8
+            default collate utf8_general_ci;""",
+            """ALTER TABLE mt_iou ADD CONSTRAINT FK_mt_iou_borrower_idx_mt_user_idx FOREIGN KEY (borrower_idx)
+             REFERENCES mt_user (idx)  ON DELETE RESTRICT ON UPDATE RESTRICT;""",
+            """ALTER TABLE mt_iou ADD CONSTRAINT FK_mt_iou_lender_idx_mt_user_idx FOREIGN KEY (lender_idx)
+             REFERENCES mt_user (idx)  ON DELETE RESTRICT ON UPDATE RESTRICT;""",
             """CREATE TABLE mt_friendlist
             (
-                `gindex`        INT    NOT NULL, 
-                `index`         INT    NOT NULL, 
-                `friend_index`  INT    NOT NULL, 
-                `credit`        INT    NULL, 
-                PRIMARY KEY (index, friend_index)
-            );""",
+                `gidx`        INT    NOT NULL, 
+                `idx`         INT    NOT NULL, 
+                `friend_idx`  INT    NOT NULL, 
+                `credit`      INT    NULL, 
+                PRIMARY KEY (idx, friend_idx)
+            )ENGINE=InnoDB
+            default character set utf8
+            default collate utf8_general_ci;""",
+            """ALTER TABLE mt_friendlist ADD CONSTRAINT FK_mt_friendlist_gidx_mt_group_gidx FOREIGN KEY (gidx)
+             REFERENCES mt_group (gidx)  ON DELETE RESTRICT ON UPDATE RESTRICT;""",
+            """ALTER TABLE mt_friendlist ADD CONSTRAINT FK_mt_friendlist_idx_mt_user_idx FOREIGN KEY (idx)
+             REFERENCES mt_user (idx)  ON DELETE RESTRICT ON UPDATE RESTRICT;""",
+            """ALTER TABLE mt_friendlist ADD CONSTRAINT FK_mt_friendlist_friend_idx_mt_user_idx FOREIGN KEY (friend_idx)
+             REFERENCES mt_user (idx)  ON DELETE RESTRICT ON UPDATE RESTRICT;""",
             """CREATE TABLE mt_grouplist
             (
-                `index`       INT    NOT NULL, 
-                `gindex`      INT    NOT NULL, 
+                `idx`         INT    NOT NULL, 
+                `gidx`        INT    NOT NULL, 
                 `permission`  INT    NULL, 
-                PRIMARY KEY (index, gindex)
-            );"""
+                PRIMARY KEY (idx, gidx)
+            )ENGINE=InnoDB
+            default character set utf8
+            default collate utf8_general_ci;""",
+            """ALTER TABLE mt_grouplist ADD CONSTRAINT FK_mt_grouplist_idx_mt_user_idx FOREIGN KEY (idx)
+             REFERENCES mt_user (idx)  ON DELETE RESTRICT ON UPDATE RESTRICT;""",
+            """ALTER TABLE mt_grouplist ADD CONSTRAINT FK_mt_grouplist_gidx_mt_group_gidx FOREIGN KEY (gidx)
+             REFERENCES mt_group (gidx)  ON DELETE RESTRICT ON UPDATE RESTRICT;"""
         ]
         for query in querys:
             self.query(query);
 
     def connect(self):
-        self.conn = sqlite3.connect("%s.db" % (self.db), isolation_level=None)
+        self.conn = pymysql.connect(host="localhost",
+                                    user="root",
+                                    password="root",
+                                    db="moneyTaker",
+                                    autocommit=True,
+                                    cursorclass=pymysql.cursors.DictCursor)
 
     def query(self, query):
         # Connection 으로부터 Cursor 생성
         self.connect()
-        with self.conn:
-            curs = self.conn.cursor()
-            curs.execute(query)
-            result = curs.fetchall()
-        self.close()
+        result = {}
+        try:
+            with self.conn.cursor() as cursor:
+                cursor.execute(query)
+                result = cursor.fetchall()
+        finally:
+            self.close()
         return result
 
     def select(self, table, args=""):
