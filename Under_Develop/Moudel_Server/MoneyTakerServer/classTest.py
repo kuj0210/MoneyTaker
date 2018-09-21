b207b6eef307a6165a31a350bb48ea23008e8b13
HoGyuneRheu <hoho0220@hanmail.net>
2018-09-22 01:43:15 +0900

    Change class function and name. json controller

--- Under_Develop/Moudel_Server/MoneyTakerServer/classTest.py
+++ Under_Develop/Moudel_Server/MoneyTakerServer/classTest.py
@@ -2,20 +2,36 @@ import json
 
 class StrLetter:
     def __init__(self, other):
-        self.keys = self.Pasing(other)
+        self._keys = self.PasingKey(other)
 
-    def Pasing(self, other):
-        return list(other)
+    def SuchKey(self, other):
+        if "List" in other:
+            return "List"
+        if "data" in other:
+            return "data"
+
+    def PasingKey(self, other):
+        keylist = list(other)
+        if ("List"in keylist) or ("data" in keylist):
+            self.PasingData(self.SuchKey(keylist), other)
+        return keylist
+
+    def PasingData(self, keyvalue, other):
+        datastring = other[keyvalue].split('\n')
+        datalist = []
+        for num in range(len(datastring)):
+            datalist.append(datastring[num].split('\t'))
+        other[keyvalue] = datalist
 
     def MakeCommad(self, other):
         totalstr = ''
-        for dickey in self.keys:
+        for dickey in self._keys:
             totalstr += str(dickey) + ':' + str(other[dickey]) + '\\n'
         return totalstr
 
-class JsonStr:
+class JsonController:
     def __init__(self, other):
-        self.ary = json.loads(other)
+        self.ary = other
         self.letter = StrLetter(self.ary)
 
     def __str__(self):
@@ -32,5 +48,39 @@ if __name__ == "__main__":
             '"type": "register", ' \
             '"id": "user1",' \
             '"pw": "UBNRE1YDUxo="}'
-    strjson = JsonStr(array)
-    print(strjson)
+    json1 = json.loads(array)
+    example1 = JsonController(json1)
+    print(example1)
+
+    array = '{ "type": "login", ' \
+            '"id": "user1", ' \
+            '"pw": "UBNRE1YDUxo="}'
+    json2 = json.loads(array)
+    example2 = JsonController(json2)
+    print(example2)
+
+    array = '{ "type" : "logout", ' \
+            '"token" : "HASH"}'
+    json3 = json.loads(array)
+    example3 = JsonController(json3)
+    print(example3)
+
+    array = '{ "type": "update-friend", ' \
+            '"token": "HASH", ' \
+            '"id": "hoho0220", ' \
+            '"data-count": "3", ' \
+            '"data": "1_on\\t연구실\\t1_on@naver.com\\n*123jejuS2\\t후배\\tjeju@kumoh.com\\nhoho0220@hanmail.net\\t후배\\thoho0220@hanmail.net"' \
+            '}'
+    json4 = json.loads(array)
+    example4 = JsonController(json4)
+    print(example4)
+
+    array = '{ "type": "delete-friend", ' \
+            '"token": "HASH", ' \
+            '"Tuple-count": "1", ' \
+            '"data-count": "1", ' \
+            '"List": "hoho0220\\t연구실\\thoho0220@hanmail.net"' \
+            '}'
+    json5 = json.loads(array)
+    example5 = JsonController(json5)
+    print(example5)
